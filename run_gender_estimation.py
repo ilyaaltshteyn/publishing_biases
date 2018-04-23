532562daa0a4f25e076806568ed74905e534298b
ilya altshteyn <ilyaaltshteyn@gmail.com>
2018-04-22 21:09:47 -0400

    first commit

---
+++ run_gender_estimation.py
@@ -0,0 +1,46 @@
+from pymongo import MongoClient
+import gender_guesser.detector as gender
+from datetime import datetime
+import logging
+from utils import get_gender, get_cursor, set_gender
+
+## Logging
+logging.basicConfig(filename='gender_estimation.log',level=logging.INFO)
+logging.info('Starting a run at time={}'.format(
+    datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    )
+)
+
+## Mongo
+client = MongoClient('mongodb://localhost:27017')
+db=client.dat
+collection = db.crossref
+
+## Gender estimation
+detector = gender.Detector(case_sensitive=False)
+
+
+# Get cursor, loop through it, estimate genders + write estimates to mongo
+results_exist = True
+finished = 0
+
+while results_exist:
+    try:
+        cursor = get_cursor()
+        if not cursor['empty']:
+            for publication in cursor['cursor']:
+                genders = []
+                for auth in publication['author']:
+                    gender = get_gender(detector, auth)
+                    genders.append(gender)
+
+                set_gender(collection, publication['_id'], genders)
+                finished += 1
+                if finished % 10000 == 0:
+                    logging.info('Finished {} records so far.'.format(finished))
+
+        else:
+            results_exist = False
+
+    except Exception, e:
+        logging.exception(e)
---
+++ utils.py
@@ -0,0 +1,56 @@
+import logging
+import re
+
+def get_gender(detector_instance, author):
+    """ Analyzes author dict to return one of several results relevant to project.
+    """
+    if not isinstance(author, dict):
+        return 'unknown-data_failure'
+
+    try:
+        if 'given' not in author or len(author['given']) == 0:
+            return 'unknown-no_first_name'
+        name = author['given'].split(' ')[0] # Take only first word
+
+        if len(name) == 1:
+            return 'unknown-single_character_name'
+
+        name_sans_initials = re.sub(r'([\w\W]{1}[.]{1})', '', name)
+        if not len(name_sans_initials):
+            return 'unknown-initials_only'
+
+        gender_estimate = d.get_gender(name)
+        if gender_estimate == 'unknown':
+            return 'unknown-estimation_failure'
+
+        return gender_estimate
+
+    except Exception, e:
+        logging.info('Was working on author string: ', str(author))
+        logging.exception(e)
+        return 'unknown-code_failure'
+
+def set_gender(collection, record_id, genders):
+    collection.update(
+        {"_id": record_id},
+        {"$set":
+            {"author-genders": genders}
+        }
+    )
+
+def get_cursor():
+    """ Get cursor pointing at records without the field 'author-genders'.
+        Return True and a cursor if cursor has at least 1 records. Otherwise
+        returns False and None. This is to avoid using .count() on big cursor.
+    """
+    c = collection.find({
+        'author-genders' : { '$exists' : False },
+        'author' : {'$exists' : True}
+        })
+
+    counter = 0
+    for record in c:
+        c.rewind()
+        # If you're in this loop there's something in the cursor
+        return {'empty' : False, 'cursor' : c}
+    return {'empty' : True, 'cursor' : c}
