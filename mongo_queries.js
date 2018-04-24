42fdcc74678d51402f8c17067fbb9d4c355bfd07
ilya altshteyn <ilyaaltshteyn@gmail.com>
2018-04-23 20:58:59 -0400

    first commit

---
+++ mongo_queries.js
@@ -0,0 +1,19 @@
+// Get just journal articles that have author genders assigned
+db.crossref.find(
+  { $and :
+    [
+      {'author-genders' : {$exists : true } },
+      {'type' : 'journal-article'}
+    ]
+  },
+  {'author-genders' : 1, 'issued' : 1, 'subject' : 1, 'author' : 1}
+)
+
+// db.crossref.find(
+//   {$and :
+//     [
+//       {'author-genders' : {$exists : false }},
+//       {'author' : {$exists : true } }
+//     ]
+//   }
+// ).count()
