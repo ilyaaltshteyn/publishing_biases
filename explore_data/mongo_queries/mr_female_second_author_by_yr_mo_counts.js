8fa421ddba05631409bb855fe9e6753e15684448
ilya altshteyn <ilyaaltshteyn@gmail.com>
2018-04-24 21:41:30 -0400

    added check for array length and presence of gender estimates

--- explore_data/mongo_queries/mr_female_second_author_by_yr_mo_counts.js
+++ explore_data/mongo_queries/mr_female_second_author_by_yr_mo_counts.js
@@ -2,7 +2,7 @@ use dat
 var mapFunction1 = function() {
   var yr = String(this.issued['date-parts'][0][0])
   var mo = String(this.issued['date-parts'][0][1])
-  if (this['author-gender'].length > 1 && this['author-gender'][1] == 'female') {
+  if ('author-gender' in this && this['author-gender'].length > 1 && this['author-gender'][1] == 'female') {
     var first_auth_female = 1
   } else {
     var first_auth_female = 0
