509bfa4431ed433b2222718a5dfd98c3e3c466a3
ilya altshteyn <ilyaaltshteyn@gmail.com>
2018-04-25 23:56:28 -0400

    added  to count names with other  gender code types

---
+++ explore_data/mongo_queries/run_queries_4.sh
@@ -0,0 +1,62 @@
+logfile="/home/ubuntu/publishing_biases/explore_data/mongo_queries/run_queries.log"
+
+function set_opts() {
+  opts="var set_xth_auth = $xth_auth, set_min_auths = $min_auths, set_gender = '$gend';"
+  sed -i "1s/.*/$opts/" mapreduce_to_yearmonth.js
+  echo "Running $opts" >> $logfile
+  mongo < mapreduce_to_yearmonth.js >> $logfile 2>&1&
+}
+
+# ---
+
+xth_auth=0
+min_auths=3
+gend="unknown-single_character_name"
+set_opts
+
+xth_auth=1
+min_auths=3
+gend="unknown-single_character_name"
+set_opts
+
+xth_auth=0
+min_auths=4
+gend="unknown-single_character_name"
+set_opts
+
+xth_auth=1
+min_auths=4
+gend="unknown-single_character_name"
+set_opts
+
+xth_auth=2
+min_auths=4
+gend="unknown-single_character_name"
+set_opts
+
+# ---
+
+xth_auth=0
+min_auths=3
+gend="unknown-initials_only"
+set_opts
+
+xth_auth=1
+min_auths=3
+gend="unknown-initials_only"
+set_opts
+
+xth_auth=0
+min_auths=4
+gend="unknown-initials_only"
+set_opts
+
+xth_auth=1
+min_auths=4
+gend="unknown-initials_only"
+set_opts
+
+xth_auth=2
+min_auths=4
+gend="unknown-initials_only"
+set_opts
