cbc17ee21e8e309751fd93ed65d6794ba8da5a44
ilya altshteyn <ilyaaltshteyn@gmail.com>
2018-04-25 23:41:31 -0400

    added run_queries_2.sh to run  gender queries

---
+++ explore_data/mongo_queries/run_queries_2.sh
@@ -0,0 +1,71 @@
+logfile="/home/ubuntu/publishing_biases/explore_data/mongo_queries/run_queries.log"
+
+function set_opts() {
+  opts="var set_xth_auth = $xth_auth, set_min_auths = $min_auths, set_gender = '$gend';"
+  sed -i "1s/.*/$opts/" mapreduce_to_yearmonth.js
+  echo "Running $opts" >> $logfile
+  mongo < mapreduce_to_yearmonth.js >> $logfile 2>&1&
+}
+
+## --- FIRST DO FEMALE NAMES ---
+# First auth, papers with >= 3 auths, female author
+xth_auth=0
+min_auths=3
+gend="mostly_female"
+set_opts
+
+# Second auth, papers with >= 3 auths, female author
+xth_auth=1
+min_auths=3
+gend="mostly_female"
+set_opts
+
+# First auth, papers with >= 4 auths, female author
+xth_auth=0
+min_auths=4
+gend="mostly_female"
+set_opts
+
+# Second auth, papers with >= 4 auths, female author
+xth_auth=1
+min_auths=4
+gend="mostly_female"
+set_opts
+
+# Third auth, papers with >= 4 auths, female author
+xth_auth=2
+min_auths=4
+gend="mostly_female"
+set_opts
+
+## --- SAME THING FOR MALE NAMES ---
+
+# First auth, papers with >= 3 auths, female author
+xth_auth=0
+min_auths=3
+gend="mostly_male"
+set_opts
+
+# Second auth, papers with >= 3 auths, female author
+xth_auth=1
+min_auths=3
+gend="mostly_male"
+set_opts
+
+# First auth, papers with >= 4 auths, female author
+xth_auth=0
+min_auths=4
+gend="mostly_male"
+set_opts
+
+# Second auth, papers with >= 4 auths, female author
+xth_auth=1
+min_auths=4
+gend="mostly_male"
+set_opts
+
+# Third auth, papers with >= 4 auths, female author
+xth_auth=2
+min_auths=4
+gend="mostly_male"
+set_opts
