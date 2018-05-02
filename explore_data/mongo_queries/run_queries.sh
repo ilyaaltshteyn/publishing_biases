bc2a75cd2959b1c1edb925a7b3410668f3a16130
ilya altshteyn <ilyaaltshteyn@gmail.com>
2018-05-01 23:22:52 -0400

    added book chapter queries

--- explore_data/mongo_queries/run_queries.sh
+++ explore_data/mongo_queries/run_queries.sh
@@ -2,10 +2,10 @@
 set -euo pipefail
 logfile="/home/ubuntu/publishing_biases/explore_data/mongo_queries/run_queries.log"
 
-function set_opts() {
-  opts="var set_xth_auth = $1, set_num_auths = $2, set_gender = '$3', set_pubtype = '$4', set_auth_count_comparison = '$5'"
-  echo "Running $opts" >> $logfile
-  cat mapreduce_to_yearmonth.js | sed -e "1s/.*/$opts/" | mongo >> $logfile 2>&1&
+function set_params() {
+  params="var set_xth_auth = $1, set_num_auths = $2, set_gender = '$3', set_pubtype = '$4', set_auth_count_comparison = '$5'"
+  echo "Running query with these params: $params" >> $logfile
+  cat mapreduce_to_yearmonth.js | sed -e "1s/.*/$params/" | mongo >> $logfile 2>&1&
 }
 
 function wait() {
@@ -19,66 +19,104 @@ function wait() {
   done
 }
 
-## Pass 4 queries at a time to mongo
+### Pass 4 queries at a time to mongo
 
-# First auth, papers with == 3 auths, female author, journal-articles
-set_opts 0 3 "female" "journal-article" "e"
+## Journal articles
 
-# Second auth, papers with == 3 auths, female author, journal-articles
-set_opts 1 3 "female" "journal-article"  "e"
+# First auth, == 3 auths, female author, journal-articles
+set_params 0 3 "female" "journal-article" "e"
 
-# First auth, papers with == 3 auths, male author, journal-articles
-set_opts 0 3 "male" "journal-article"  "e"
+# Second auth, == 3 auths, female author, journal-articles
+set_params 1 3 "female" "journal-article"  "e"
 
-# Second auth, papers with == 3 auths, male author, journal-articles
-set_opts 1 3 "male" "journal-article"  "e"
+# First auth, == 3 auths, male author, journal-articles
+set_params 0 3 "male" "journal-article"  "e"
 
-wait()
-
-# First auth, papers with == 4 auths, female author, journal-articles
-set_opts 0 4 "female" "journal-article"  "e"
-
-# Second auth, papers with == 4 auths, female author, journal-articles
-set_opts 1 4 "female" "journal-article"  "e"
-
-# Third auth, papers with == 4 auths, female author, journal-articles
-set_opts 2 4 "female" "journal-article"  "e"
-
-# First auth, papers with == 4 auths, male author, journal-articles
-set_opts 0 4 "male" "journal-article"  "e"
+# Second auth, == 3 auths, male author, journal-articles
+set_params 1 3 "male" "journal-article"  "e"
 
 wait()
 
-# Second auth, papers with == 4 auths, male author, journal-articles
-set_opts 1 4 "male" "journal-article"  "e"
+# First auth, == 4 auths, female author, journal-articles
+set_params 0 4 "female" "journal-article"  "e"
 
-# Third auth, papers with == 4 auths, male author, journal-articles
-set_opts 2 4 "male" "journal-article"  "e"
+# Second auth, == 4 auths, female author, journal-articles
+set_params 1 4 "female" "journal-article"  "e"
 
-# First auth, papers with >= 5 auths, female author, journal-articles
-set_opts 0 5 "female" "journal-article"  "ge"
+# Third auth, == 4 auths, female author, journal-articles
+set_params 2 4 "female" "journal-article"  "e"
 
-# Second auth, papers with >= 5 auths, female author, journal-articles
-set_opts 1 5 "female" "journal-article"  "ge"
-
-wait()
-
-# Third auth, papers with >= 5 auths, female author, journal-articles
-set_opts 2 5 "female" "journal-article"  "ge"
-
-# Fourth auth, papers with >= 5 auths, female author, journal-articles
-set_opts 3 5 "female" "journal-article"  "ge"
-
-# First auth, papers with >= 5 auths, male author, journal-articles
-set_opts 0 5 "male" "journal-article"  "ge"
-
-# Second auth, papers with >= 5 auths, male author, journal-articles
-set_opts 1 5 "male" "journal-article"  "ge"
+# First auth, == 4 auths, male author, journal-articles
+set_params 0 4 "male" "journal-article"  "e"
 
 wait()
 
-# Third auth, papers with >= 5 auths, male author, journal-articles
-set_opts 2 5 "male" "journal-article"  "ge"
+# Second auth, == 4 auths, male author, journal-articles
+set_params 1 4 "male" "journal-article"  "e"
 
-# Fourth auth, papers with >= 5 auths, male author, journal-articles
-set_opts 3 5 "male" "journal-article"  "ge"
+# Third auth, == 4 auths, male author, journal-articles
+set_params 2 4 "male" "journal-article"  "e"
+
+# First auth, >= 5 auths, female author, journal-articles
+set_params 0 5 "female" "journal-article"  "ge"
+
+# Second auth, >= 5 auths, female author, journal-articles
+set_params 1 5 "female" "journal-article"  "ge"
+
+wait()
+
+# Third auth, >= 5 auths, female author, journal-articles
+set_params 2 5 "female" "journal-article"  "ge"
+
+# Fourth auth, >= 5 auths, female author, journal-articles
+set_params 3 5 "female" "journal-article"  "ge"
+
+# First auth, >= 5 auths, male author, journal-articles
+set_params 0 5 "male" "journal-article"  "ge"
+
+# Second auth, >= 5 auths, male author, journal-articles
+set_params 1 5 "male" "journal-article"  "ge"
+
+wait()
+
+# Third auth, >= 5 auths, male author, journal-articles
+set_params 2 5 "male" "journal-article"  "ge"
+
+# Fourth auth, >= 5 auths, male author, journal-articles
+set_params 3 5 "male" "journal-article"  "ge"
+
+## Book chapters
+
+# First auth, == 3 auths, female author, book-chapter
+set_params 0 3 "female" "book-chapter" "e"
+
+# Second auth, == 3 auths, female author, book-chapter
+set_params 1 3 "female" "book-chapter"  "e"
+
+wait()
+
+# First auth, == 3 auths, male author, book-chapter
+set_params 0 3 "male" "book-chapter"  "e"
+
+# Second auth, == 3 auths, male author, book-chapter
+set_params 1 3 "male" "book-chapter"  "e"
+
+# First auth, >= 4 auths, female author, book-chapter
+set_params 0 4 "female" "book-chapter" "ge"
+
+# Second auth, >= 4 auths, female author, book-chapter
+set_params 1 4 "female" "book-chapter"  "ge"
+
+wait()
+
+# Third auth, >= 4 auths, female author, book-chapter
+set_params 2 4 "female" "book-chapter"  "ge"
+
+# First auth, >= 4 auths, male author, book-chapter
+set_params 0 4 "male" "book-chapter"  "ge"
+
+# Second auth, >= 4 auths, male author, book-chapter
+set_params 1 4 "male" "book-chapter"  "ge"
+
+# Third auth, >= 4 auths, male author, book-chapter
+set_params 2 4 "male" "book-chapter"  "ge"
