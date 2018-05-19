91f840e406f142e76cc0fe3d6a70943d3816177a
ilya altshteyn <ilyaaltshteyn@gmail.com>
2018-05-19 11:55:56 -0400

    new exports file

---
+++ analyze/export_utils/exports2.sh
@@ -0,0 +1,12 @@
+mongoexport -d dat -c female_0th_auth_book_3or_more_auths_per_yr --type=csv --fields _id,value --out female_0th_auth_book_3or_more_auths_per_yr.csv
+mongoexport -d dat -c female_1th_auth_book_3or_more_auths_per_yr --type=csv --fields _id,value --out female_1th_auth_book_3or_more_auths_per_yr.csv
+mongoexport -d dat -c male_0th_auth_book_3or_more_auths_per_yr --type=csv --fields _id,value --out male_0th_auth_book_3or_more_auths_per_yr.csv
+mongoexport -d dat -c male_1th_auth_book_3or_more_auths_per_yr --type=csv --fields _id,value --out male_1th_auth_book_3or_more_auths_per_yr.csv
+mongoexport -d dat -c female_0th_auth_dataset_3or_more_auths_per_yr --type=csv --fields _id,value --out female_0th_auth_dataset_3or_more_auths_per_yr.csv
+mongoexport -d dat -c female_1th_auth_dataset_3or_more_auths_per_yr --type=csv --fields _id,value --out female_1th_auth_dataset_3or_more_auths_per_yr.csv
+mongoexport -d dat -c male_0th_auth_dataset_3or_more_auths_per_yr --type=csv --fields _id,value --out male_0th_auth_dataset_3or_more_auths_per_yr.csv
+mongoexport -d dat -c male_1th_auth_dataset_3or_more_auths_per_yr --type=csv --fields _id,value --out male_1th_auth_dataset_3or_more_auths_per_yr.csv
+mongoexport -d dat -c female_0th_auth_proceedings-article_3or_more_auths_per_yr --type=csv --fields _id,value --out female_0th_auth_proceedings-article_3or_more_auths_per_yr.csv
+mongoexport -d dat -c female_1th_auth_proceedings-article_3or_more_auths_per_yr --type=csv --fields _id,value --out female_1th_auth_proceedings-article_3or_more_auths_per_yr.csv
+mongoexport -d dat -c male_0th_auth_proceedings-article_3or_more_auths_per_yr --type=csv --fields _id,value --out male_0th_auth_proceedings-article_3or_more_auths_per_yr.csv
+mongoexport -d dat -c male_1th_auth_proceedings-article_3or_more_auths_per_yr --type=csv --fields _id,value --out male_1th_auth_proceedings-article_3or_more_auths_per_yr.csv
